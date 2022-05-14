fdb08bb90cb7d849cab425865bb3eee37b6c5df0
Ajten <ajtencani@gmail.com>
2022-05-14 19:27:15 +0200

    testing team swapping colors

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { Label, Text, Circle } from "react-konva";
 
 function Player(props) {
-  return (
-      <Label draggable x={props.width * 0.5} y={props.height * 0.5}>
-          <Circle radius={50} width={30} height={30} fill="red" />
-          <Text draggable x={-10} y={20} text="tope" />
-      </Label>
-  )
+    const handleDragStart = () => {
+
+    }
+    const handleDragEnd = (e) => {
+        console.log(e.target.x() < props.width * 0.5);
+        if (e.target.x() <= props.width * 0.5){
+            e.target.children[0].attrs.fill = 'white';
+        } else {
+            e.target.children[0].attrs.fill = 'black';
+        } 
+    }
+    return (
+        <Label draggable x={props.width * 0.5} y={props.height * 0.5} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+            <Circle radius={50} width={30} height={30} fill="red" style={{border:'2px solid black'}} />
+            <Text draggable x={-10} y={20} text="tope" />
+        </Label>
+    )
 }
 
 export default Player
