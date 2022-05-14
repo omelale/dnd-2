6ca851b49f258b84f0f73c4654686b1f5407879a
Ajten <ajtencani@gmail.com>
2022-05-15 01:35:28 +0200

    prevent player from going outside of canvas

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Label, Text, Circle } from "react-konva";
 
 function Player(props) {
+    const [x, setX] = useState(props.width * 0.5);
+    const [y, setY] = useState(props.height * 0.5);
     const handleDragStart = () => {
 
     }
@@ -12,8 +14,28 @@ function Player(props) {
             e.target.children[0].attrs.fill = 'black';
         } 
     }
+    const handleDragMove = (e) => {
+        const currentX = e.evt.clientX;
+        const currentY = e.evt.clientY;
+        console.log(e.target._lastPos.x);
+        
+        if (currentX < 0 ){
+            e.target.attrs.x = 20;
+            e.target.attrs.y = e.target._lastPos.y;
+        }
+        if (currentY < 0){
+            e.target.attrs.y = 20;
+            e.target.attrs.x = e.target._lastPos.x;
+        }
+        if (currentX > props.width){
+            e.target.attrs.x = props.width - 20;
+        }
+        if (currentY > props.height){
+            e.target.attrs.y = props.height - 20;
+        }
+    }
     return (
-        <Label draggable x={props.width * 0.5} y={props.height * 0.5} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+        <Label draggable x={x} y={y} onDragStart={handleDragStart} onDragEnd={handleDragEnd} onDragMove={handleDragMove}>
             <Circle radius={50} width={30} height={30} fill="red" style={{border:'2px solid black'}} />
             <Text draggable x={-10} y={20} text="tope" />
         </Label>
