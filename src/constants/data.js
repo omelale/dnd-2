d3a05c2fe256df651e2bb152778acfe0ac5ed743
Ajten <ajtencani@gmail.com>
2022-05-15 22:37:47 +0200

    initialized data

---
+++ src/constants/data.js
@@ -0,0 +1,68 @@
+const teams = 2;
+
+const playersPerTeam = 6;
+
+const players = [
+    {
+        id: 1,
+        name: 'Shaban',
+        lastName: 'R',
+    },
+    {
+        id: 2,
+        name: 'Andi',
+        lastName: 'X',
+    },
+    {
+        id: 3,
+        name: 'Ajten',
+        lastName: 'C',
+    },
+    {
+        id: 4,
+        name: 'Fabian',
+        lastName: 'E',
+    },
+    {
+        id: 5,
+        name: 'Erik',
+        lastName: 'K',
+    },
+    {
+        id: 6,
+        name: 'Bruno',
+        lastName: 'B',
+    },
+    {
+        id: 7,
+        name: 'Aris',
+        lastName: 'K',
+    },
+    {
+        id: 8,
+        name: 'Danjel',
+        lastName: 'B',
+    },
+    {
+        id: 9,
+        name: 'Aldo',
+        lastName: 'I',
+    },
+    {
+        id: 10,
+        name: 'Albi',
+        lastName: 'X',
+    },
+    {
+        id: 11,
+        name: 'Erion',
+        lastName: 'E',
+    },
+    {
+        id: 12,
+        name: 'Ketjon',
+        lastName: 'F',
+    },
+
+];
+export default { teams, playersPerTeam, players }
